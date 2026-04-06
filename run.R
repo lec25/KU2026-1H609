b5a1f52294849efb3ed9d595a1016df44a1d01fb
yz-apr <zhouyi0426@hotmail.com>
2026-04-06 18:05:46 +0900

    update

--- run.R
+++ run.R
@@ -1,2 +1,8 @@
 bookdown::render_book()
 bookdown::serve_book()
+bookdown::gitbook()
+
+bookdown::create_gitbook("C:\\Users\\zhouy\\Documents\\Computer\\KU2026-1H609")
+
+
+bookdown::render_book("index.Rmd")
